6d18f609aa0a867e48387be996934892926c2dce
eugene-benitez <eugenebenitez@protonmail.com>
2019-06-01 21:23:56 -0700

    Started on FoundationsII algorithms.

---
+++ Fundamentals/Fundamentals.js
@@ -0,0 +1,26 @@
+//1. Sigma :
+function sigma(x) {
+	var sum = 0;
+	for (var i = 1; i <= x; i++) {
+		sum += i;
+	}
+	console.log(sum);
+}
+
+//sigma(3);
+
+//2. Factorial :
+function factorial(x) {
+	var sum = 1;
+	for (var i = 1; i <= x; i++) {
+		sum *= i;
+	}
+	console.log(sum);
+}
+
+//factorial(5);
+
+//3. Star Art : 
+function starArt() {
+	
+}
