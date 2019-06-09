77dd028d0f60c5f8b5e9fc7c70048cb046b966b4
eugene-benitez <eugenebenitez@protonmail.com>
2019-06-08 18:27:11 -0700

    Fixed merge bug. Fundamental files were not included.

---
+++ Fundamentals/Fundamentals.js
@@ -0,0 +1,69 @@
+//1. Sigma :
+function sigma(x) {
+	var sum = 0;
+	for (var i = 1; i <= x; i++) {
+		sum += i;
+	}
+	console.log(sum);
+}
+
+//sigma(3);
+
+//2. Factorial :
+function factorial(x) {
+	var sum = 1;
+	for (var i = 1; i <= x; i++) {
+		sum *= i;
+	}
+	console.log(sum);
+}
+
+//factorial(5);
+
+//3. Star Art : 
+function drawLeftStars(x = 5) {
+	var str = "";
+	var i = 1;
+	while (i <= x) {
+		str += "*";
+		i++;
+	}
+	console.log(str);
+}
+
+drawLeftStars();
+
+function drawRightStars(x = 5) {
+	var str = "";
+	var spaces = 75 - x;
+	for (var i = 1; i <= spaces; i++) {
+		str += ".";
+	}
+	for (var j = 1; j <= x; j++) {
+		str += "*";
+	}
+	console.log(str);
+	//	console.log(str.length);
+}
+
+drawRightStars();
+
+function drawCenteredStars(x = 15) {
+	var str = "";
+	var spaces = 75 - (x * 2) - x;
+	console.log(spaces);
+	for (var i = 1; i <= spaces; i++) {
+		str += ".";
+	}
+
+	for (var j = 1; j <= x; j++) {
+		str += "*";
+	}
+	for (var k = 1; k <= spaces; k++) {
+		str += ".";
+	}
+	console.log(str);
+}
+
+drawCenteredStars();
+
---
+++ Fundamentals/Fundamentals2.js
@@ -0,0 +1,46 @@
+//// 1. “Create threesFives() that adds values from 100 and 4000000 (inclusive) if that value is evenly divisible by 3 or 5 but not both. Display the final sum in the console.
+
+function threesFives(num1, num2) {
+    var sum = 0;
+    for (var i = num1; i <= num2; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            break;
+        }
+
+        if (i % 3 == 0 | i % 5 == 0) {
+            sum += i;
+        }
+    }
+    console.log(sum);
+}
+
+//threesFives(100,4000000);
+
+
+
+///// 2. Generate Coin Change
+
+//3. Messy Math Mashup
+
+function messyMath(num) {
+    var sum = 0;
+    for (var i = 0; i <= num; i++) {
+        if (num / 3 == i) {
+            return -1;
+        }
+        if (i % 3 == 0) {
+            break;
+        }
+        if (i % 7 == 0) {
+            sum += i;
+            sum += i;
+            break;
+        }
+        else {
+            sum += i;
+        }
+    }
+    console.log(sum);
+}
+
+messyMath(10);
