e0f35be9583d95acedbf2d9b96a9a2d0d5b35488
eugene-benitez <eugenebenitez@protonmail.com>
2019-06-13 13:48:20 -0700

    Added Array alogirthms file and pushFront method.

---
+++ Fundamentals/Arrays.js
@@ -0,0 +1,22 @@
+//1. Push Front
+var testArr = [1,2,3,4];
+
+function swapIndices(idx1, idx2, arr) {
+	var temp = arr[idx1];
+	arr[idx1] = arr[idx2];
+	arr[idx2] = temp;
+	console.log("swap is", arr)
+	return arr;
+}
+
+function pushFront(arr,val) {
+	var lastIndex = arr.length;
+	arr.push(val);
+	console.log(arr);
+	for (var i = 0; i <= lastIndex; i++) {
+		swapIndices(i, lastIndex, arr);
+		console.log(arr);
+	}
+}
+
+pushFront(testArr, 0);
